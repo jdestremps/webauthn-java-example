d8c7bdcea09dc704213eb82dc59bbe66ef74e326
Jerry Destremps <jdestremps@gmail.com>
2025-05-04 14:02:52 -0700

    Added client side logging

--- src/main/resources/static/javascript/registerauth.js
+++ src/main/resources/static/javascript/registerauth.js
@@ -1,53 +1,66 @@
 document.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    fetch('/register', {
-        method: 'POST',
-        body: formData
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  fetch('/register', {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => {
+      console.log("response1: ", response);
+      return initialCheckStatus(response)
     })
-    .then(response => initialCheckStatus(response))
-    .then(credentialCreateJson => ({
+    .then(credentialCreateJson => {
+      console.log("credentialCreateJson: ", credentialCreateJson);
+      return {
         publicKey: {
-            ...credentialCreateJson.publicKey,
-            challenge: base64urlToUint8array(credentialCreateJson.publicKey.challenge),
-        user: {
+          ...credentialCreateJson.publicKey,
+          challenge: base64urlToUint8array(credentialCreateJson.publicKey.challenge),
+          user: {
             ...credentialCreateJson.publicKey.user,
             id: base64urlToUint8array(credentialCreateJson.publicKey.user.id),
-        },
-        excludeCredentials: credentialCreateJson.publicKey.excludeCredentials.map(credential => ({
+          },
+          excludeCredentials: credentialCreateJson.publicKey.excludeCredentials.map(credential => ({
             ...credential,
             id: base64urlToUint8array(credential.id),
-        })),
-        extensions: credentialCreateJson.publicKey.extensions,
+          })),
+          extensions: credentialCreateJson.publicKey.extensions,
         },
-    }))
-    .then(credentialCreateOptions =>
-        navigator.credentials.create(credentialCreateOptions))
-    .then(publicKeyCredential => ({
+      };
+    })
+    .then(credentialCreateOptions => {
+      console.log("credentialCreateOptions: ", credentialCreateOptions);
+      return navigator.credentials.create(credentialCreateOptions);
+    })
+    .then(publicKeyCredential => {
+      console.log("publicKeyCredential ");
+      return {
         type: publicKeyCredential.type,
         id: publicKeyCredential.id,
         response: {
-        attestationObject: uint8arrayToBase64url(publicKeyCredential.response.attestationObject),
-        clientDataJSON: uint8arrayToBase64url(publicKeyCredential.response.clientDataJSON),
-        transports: publicKeyCredential.response.getTransports && publicKeyCredential.response.getTransports() || [],
+          attestationObject: uint8arrayToBase64url(publicKeyCredential.response.attestationObject),
+          clientDataJSON: uint8arrayToBase64url(publicKeyCredential.response.clientDataJSON),
+          transports: publicKeyCredential.response.getTransports && publicKeyCredential.response.getTransports() || [],
         },
         clientExtensionResults: publicKeyCredential.getClientExtensionResults(),
-    }))
+      };
+    })
     .then((encodedResult) => {
-        console.log("encodedResult: ", JSON.stringify(encodedResult));
-        const form = document.getElementById("form");
-        const formData = new FormData(form);
-        console.log("formData: ", formData);
-        formData.append("credential", JSON.stringify(encodedResult));
-        return fetch("/finishauth", {
-            method: 'POST',
-            body: formData
-        })
+      console.log("encodedResult: ", JSON.stringify(encodedResult));
+      const form = document.getElementById("form");
+      const formData = new FormData(form);
+      console.log("formData: ", formData);
+      formData.append("credential", JSON.stringify(encodedResult));
+      return fetch("/finishauth", {
+        method: 'POST',
+        body: formData
+      })
     })
     .then((response) => {
-        followRedirect(response);
+      console.log("response2: ", response);
+      return followRedirect(response);
     })
     .catch((error) => {
-        displayError(error);
+      console.log('Error: ', error);
+      return displayError(error);
     });
 })
